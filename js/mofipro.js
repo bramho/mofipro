b2b70d162dd189d5801633b2a4ce4292d8469d82
bramho <bramhoningh16@gmail.com>
2016-06-02 13:43:41 +0200

    Added JavaScript/jQuery to rating module

---
+++ js/mofipro.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+   var ratings = new Array();
+   var selectedStar = 0;
+
+   $('.rating-stars:radio').change(function() {
+      selectedStar = $(this).data('star');
+   });
+
+   $('#submit-rating').on('click', function() {
+      var rating = selectedStar;
+
+      if (rating == 0) {
+         alert('Please select a star');
+      } else {
+         ratings.push(rating);
+
+         var sum = 0;
+
+         for (var i = 0; i < ratings.length; i++) {
+            sum += ratings[i];
+         }
+
+         var average = sum / ratings.length;
+
+         if ( Math.round(average) !== average ) {
+            average = average.toFixed(1);
+         }
+
+         $('.rating-output').html(average);
+      }
+
+   });
+
+
+
+});
